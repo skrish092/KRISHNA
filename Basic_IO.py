bdc9b88ef694cd019b19da107fc1b264a9924c82
Esukapalli Siva Krishna Reddy <skrish092@gmail.com>
2024-05-27 10:53:36 +0530

    Testing file for git

---
+++ Basic_IO.py
@@ -0,0 +1,12 @@
+eno=int(input("Enter employ number"))
+ename=input("Enter employ name")
+job=input("Enter job")
+sal=float(input("Enter salary"))
+dno=int(input("Enter department no"))
+
+print("The employ details are...")
+print("Eno=",eno)
+print("Ename=",ename)
+print("Job=",job)
+print("Sal=",sal)
+print("Dno=",dno)
